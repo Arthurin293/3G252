a3ea63a103104257e3108627553ed9be29aae4dd
Arthurin293 <a24310293@ceti.mx>
2025-09-06 01:02:12 +0000

    practica 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include <iostream>
+#include <Foco.hpp>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"Hola mundo"<<endl;
+    Foco foquito;
+    foquito.Encender();
+    foquito.Apagar();
     return 0;
 }
